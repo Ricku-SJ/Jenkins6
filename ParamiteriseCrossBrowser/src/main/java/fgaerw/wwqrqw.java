fbc40f54d91e5c107bc45a286e2a6093e5130341
Ricku-SJ <srinibashjenajnn99@gmail.com>
2023-09-05 22:55:19 +0530

    second commit

--- ParamiteriseCrossBrowser/src/main/java/fgaerw/wwqrqw.java
+++ ParamiteriseCrossBrowser/src/main/java/fgaerw/wwqrqw.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -39,6 +40,11 @@ public class wwqrqw {
 		{
 			WebDriverManager.firefoxdriver().setup();
 			driver=new FirefoxDriver();
+		}
+		else if(browser.equals("ie"))
+		{
+			WebDriverManager.iedriver().setup();
+			driver=new  InternetExplorerDriver();
 		}
 	}
 	@Parameters({"url"})
